2c6fe08a484e52f513fee3cc8f96c8d353e77377
Kevin Mershon <kevin@slixbits.com>
2014-07-08 11:14:08 -0700

    Add basic Clojure project structure

---
+++ Phase II/WebService/web-service/project.clj
@@ -0,0 +1,10 @@
+(defproject web-service "0.1.0-SNAPSHOT"
+  :description "REST API for hydra-core"
+  :url "slixbits.com"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.8"]]
+  :plugins [[lein-ring "0.8.11"]]
+  :ring {:handler web-service.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
