9c1b832c6b2f51c66c75263d4f9db0541db43d9c
Kevin Mershon <kevin@slixbits.com>
2014-07-09 15:15:47 -0700

    Add JDBC and PostgreSQL to the app

--- Phase II/WebService/web-service/project.clj
+++ Phase II/WebService/web-service/project.clj
@@ -2,8 +2,10 @@
   :description "REST API for hydra-core"
   :url "slixbits.com"
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/java.jdbc "0.3.4"]
                  [compojure "1.1.8"]
-                 [ring/ring-json "0.3.1"]]
+                 [ring/ring-json "0.3.1"]
+                 [postgresql "9.3-1101.jdbc4"]]
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler web-service.handler/app}
   :profiles
--- Phase II/WebService/web-service/src/web_service/handler.clj
+++ Phase II/WebService/web-service/src/web_service/handler.clj
@@ -3,7 +3,21 @@
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
             [ring.middleware.json :as middleware]
+            [clojure.java.jdbc :as sql]
             [compojure.route :as route]))
+
+(let [db-host "localhost"
+      db-port 5432
+      db-name "postgres"
+      db-schema "public"]
+
+  (def db {:classname "org.postgresql.Driver"
+           :subprotocol "postgresql"
+           :subname  (str "//" db-host ":" db-port "/" db-name)
+           ; Any additional keys are passed to the driver
+           ; as driver-specific properties.
+           :user "postgres"
+           :password "password"}))
 
 (defn get-version
   []
