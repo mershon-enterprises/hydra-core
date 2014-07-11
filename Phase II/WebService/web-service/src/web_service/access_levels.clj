2f5961cbd3ce08cbda42dfecc103bbcc18591e7d
Kevin Mershon <kevin@slixbits.com>
2014-07-11 10:38:02 -0700

    Add REST APIs for access levels

---
+++ Phase II/WebService/web-service/src/web_service/access_levels.clj
@@ -0,0 +1,46 @@
+(ns web-service.access-levels
+  (:use [ring.util.response]
+        [web-service.db])
+  (:require [clojure.java.jdbc :as sql]
+            [compojure.route :as route]))
+
+; get the specified access level
+(defn access-level-get
+  [description]
+  (let
+    [access-level (first
+                    (sql/query
+                      db
+                      ["select * from public.user_access_level where description=?" description]))]
+    (if access-level
+      (response access-level)
+      (not-found "Access Level not found"))))
+
+; add the specified permission to the user
+(defn access-level-add
+  [description]
+  (let
+    [success (try
+               (sql/execute!
+                 db
+                 ["insert into public.user_access_level (description) values (?)"
+                  description])
+               true
+               (catch Exception e
+                 (println (.getMessage e))
+                 false))]
+    ; if we successfully created the access level, return a "created" status and
+    ; invoke access-level-get
+    ; otherwise, return a "conflict" status
+    (if success
+      (status (access-level-get description) 201)
+      (status {:body "Access Level already exists"} 409))))
+
+; list the access-levels in the database
+(defn access-level-list
+  []
+  (response
+    (sql/query
+      db
+      ["select * from public.user_access_level"]
+      :row-fn :description)))
--- Phase II/WebService/web-service/src/web_service/handler.clj
+++ Phase II/WebService/web-service/src/web_service/handler.clj
@@ -1,11 +1,11 @@
 (ns web-service.handler
   (:use [ring.util.response]
-        [web-service.user])
+        [web-service.user]
+        [web-service.access-levels])
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
             [ring.middleware.json :as middleware]
-            [compojure.route :as route]
-            [web-service.user :as user]))
+            [compojure.route :as route]))
 
 ; get the version of the API
 (defn get-version
@@ -15,12 +15,27 @@
 (defroutes app-routes
   (GET "/version" [] (get-version))
 
-  (POST "/user/access/add"
-        [email_address access_level]
-        (user-access-add email_address access_level))
-  (GET "/user/access/list"
-        [email_address]
-       (user-access-list email_address))
+  (context
+    "/access-levels" []
+    (defroutes document-routes
+      (GET "/" [] (access-level-list))
+      (PUT "/" [] (status {:body "Update-all access levels not allowed"} 403))
+      (POST "/" [description] (access-level-add description))
+      (DELETE "/" [] (status {:body "Delete-all access levels not allowed"} 403))
+      (context
+        "/:description" [description]
+        (defroutes document-routes
+          (GET "/" [] (access-level-get description))
+          (PUT "/" [] (status {:body "Update access level not implemented"} 405))
+          (POST "/" [] (access-level-add description))
+          (DELETE "/" [] (status {:body "Delete access level not allowed"} 403))))))
+
+  ; (POST "/user/access/add"
+  ;       [email_address access_level]
+  ;       (user-access-add email_address access_level))
+  ; (GET "/user/access/list"
+  ;       [email_address]
+  ;      (user-access-list email_address))
 
   (context
     "/users" []
