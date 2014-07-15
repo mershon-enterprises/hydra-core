155195c30db314a4f3d78f7e5a087a9cad9169d6
Kevin Mershon <kevin@slixbits.com>
2014-07-15 12:24:02 -0700

    Fix unnecessary response wrapping for adding user access

--- Phase II/WebService/web-service/src/web_service/user.clj
+++ Phase II/WebService/web-service/src/web_service/user.clj
@@ -72,30 +72,29 @@
 ; add the specified permission to the user
 (defn user-access-add
   [email-address access-level]
-  (response
-    (let
-      [success (try
-                 (sql/execute!
-                   db
-                   [(str "insert into public.user_to_user_access_level "
-                         "(user_id, access_level_id) "
-                         "values ("
-                         "(select id from public.user where email_address=?), "
-                         "(select id from public.user_access_level where description=?))")
-                    email-address access-level])
-                 true
-                 (catch Exception e
-                   (println (.getMessage e))
-                   (println (.getMessage (.getNextException e)))
-                   false))]
+  (let
+    [success (try
+               (sql/execute!
+                 db
+                 [(str "insert into public.user_to_user_access_level "
+                       "(user_id, access_level_id) "
+                       "values ("
+                       "(select id from public.user where email_address=?), "
+                       "(select id from public.user_access_level where description=?))")
+                  email-address access-level])
+               true
+               (catch Exception e
+                 (println (.getMessage e))
+                 (println (.getMessage (.getNextException e)))
+                 false))]
 
-      ; if we successfully created the user access level, return a "created"
-      ; status and invoke user-get
-      ; otherwise, return a "conflict" status
-      (if success
-        (status (user-access-list email-address) 201)
-        (status {:body (str "User access for "
-                            email-address
-                            " already exists: "
-                            access-level)}
-                409)))))
+    ; if we successfully created the user access level, return a "created"
+    ; status and invoke user-get
+    ; otherwise, return a "conflict" status
+    (if success
+      (status (user-access-list email-address) 201)
+      (status {:body (str "User access for "
+                          email-address
+                          " already exists: "
+                          access-level)}
+              409))))
