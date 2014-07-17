c2b377cebbbd1a8c5f051a61105aa1b6aca88cfc
Kevin Mershon <kevin@slixbits.com>
2014-07-17 12:35:02 -0700

    Add new constants for access levels relating to data

--- Phase II/WebService/web-service/src/web_service/constants.clj
+++ Phase II/WebService/web-service/src/web_service/constants.clj
@@ -1,6 +1,15 @@
 (ns web-service.constants)
 
+(def create-data "Create Attachments")
+(def create-data "Create Data")
+
+(def manage-clients "Manage Attachments")
 (def manage-clients "Manage Clients")
+(def create-data "Manage Data")
 (def manage-users "Manage Users")
 
+(def view-attachments "View Attachments")
 (def view-clients "View Clients")
+(def view-same-client-data "View Same Client Data")
+(def view-same-client-location-data "View Same Client Location Data")
+(def view-own-data "View Own Data")
