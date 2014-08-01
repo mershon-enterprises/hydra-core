d8705c591ad78209489da0be960cb16fc0545c2c
Kevin Mershon <kevin@slixbits.com>
2014-08-01 10:00:28 -0700

    Start a commonjs project with the help of Yeoman and Grunt

---
+++ Phase II/RestClient/src/restclient.js
@@ -0,0 +1,17 @@
+/*
+ * RestClient
+ * https://github.com/Slixbits/hydra-core
+ *
+ * Copyright (c) 2014 Kevin Mershon
+ * Licensed under the GPLv3 license.
+ */
+
+(function(exports) {
+
+  'use strict';
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
