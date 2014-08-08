38f4b8eedcd4c0ffeb045a045a650f51bcd2c1ec
Kevin Mershon <kevin@slixbits.com>
2014-08-08 09:46:36 -0700

    Add the crypto-random library

--- Phase II/WebService/web-service/project.clj
+++ Phase II/WebService/web-service/project.clj
@@ -9,7 +9,8 @@
                  [org.clojure/data.json "0.2.5"]
                  [org.clojars.pntblnk/clj-ldap "0.0.9"]
                  [org.clojure/tools.logging "0.3.0"]
-                 [com.draines/postal "1.11.1"]]
+                 [com.draines/postal "1.11.1"]
+                 [crypto-random "1.2.0"]]
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler web-service.handler/app}
   :profiles
