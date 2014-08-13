1f5c9a9fd0a1440e4175f92b5928ba6c8667284e
Kevin Mershon <kevin@slixbits.com>
2014-08-13 15:56:03 -0700

    Refactor session start/log logic

--- Phase II/WebService/web-service/src/web_service/session.clj
+++ Phase II/WebService/web-service/src/web_service/session.clj
@@ -13,40 +13,71 @@
     401))
 
 
-; get the current session id
-(defn- get-current-id
-  [session]
-  (let [query (str "select us.id from public.user_session us "
-                   "inner join public.user u "
-                   "  on u.id = us.user_id "
-                   "where us.end_date is null and u.email_address=?")]
-    (first (sql/query db [query (:email-address session)] :row-fn :id))))
-
-
-; log some arbitrary detail about a session
-(defn log-detail
-  [session attribute value]
-  (let [query (str "insert into public.user_session_detail "
-                   "(attribute, value, session_id) values (?,?,?)")
-        session-id (get-current-id session)]
-    (try (sql/execute! db [query attribute value (get-current-id session)])
-         true
-         (catch Exception e
-           (.printStackTrace e)
-           false))))
-
-
-; start a new session
+; start a new session, and immediately end it
+;
+; the reason we end the session immediately is because a session is considered
+; to still be active for up to 30 minutes but we don't want to leave the end
+; date null forever
 (defn start
   [email-address]
 
-  ; now, start a new session
-  (let [query (str "insert into public.user_session (start_date, user_id) "
-                   "values (now(), "
+  (let [query (str "insert into public.user_session "
+                   "(start_date, end_date, user_id) "
+                   "values (now(), now(), "
                    "  (select id from public.user where email_address=?)"
                    ")")]
     (try (sql/execute! db [query email-address])
          true
          (catch Exception e
            (println (.getMessage e))
+           false))))
+
+
+; get the current session id
+(defn- get-current-id
+  [email-address]
+  (let [get-query (str "select us.id from public.user_session us "
+                       "inner join public.user u "
+                       "  on u.id = us.user_id "
+                       "where u.email_address=? "
+                       "and ("
+                       "  us.end_date is null"
+                       "  or (now() - us.end_date) < interval '30 minutes'"
+                       ")")
+        update-query (str "update public.user_session "
+                          "set end_date=now(), date_modified=now() "
+                          "where id=?")
+        get-current (fn []
+                      (first (sql/query db
+                                        [get-query email-address]
+                                        :row-fn :id)))
+        update-current (fn [current-id]
+                         (try (sql/execute! db [update-query current-id])
+                              (catch Exception e
+                                (.printStackTrace e))))
+        current-id (get-current)]
+
+    ; if there isn't a current session, start one
+    (if current-id
+      (do
+        (update-current current-id)
+        current-id)
+      (do
+        (start email-address)
+        (get-current)))))
+
+
+; log some arbitrary detail about a session
+(defn log-detail
+  [email-address attribute value]
+  (let [query (str "insert into public.user_session_detail "
+                   "(attribute, value, session_id) values (?,?,?)")
+        session-id (get-current-id email-address)]
+    (try (sql/execute! db [query
+                           attribute
+                           value
+                           (get-current-id email-address)])
+         true
+         (catch Exception e
+           (.printStackTrace e)
            false))))
