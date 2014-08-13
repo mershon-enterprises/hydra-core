e01f139ec0240ec6dd0171af1db8845320bc7e68
Kevin Mershon <kevin@slixbits.com>
2014-08-13 15:54:29 -0700

    Get rid of now-unused has-access function

--- Phase II/WebService/web-service/src/web_service/session.clj
+++ Phase II/WebService/web-service/src/web_service/session.clj
@@ -3,12 +3,6 @@
         [web-service.db])
   (:require [clojure.java.jdbc :as sql]))
 
-; check if the logged-in user has the specified access level associated with
-; their account
-(defn has-access
-  [session access-level]
-  (let [session-access (set (:access session))]
-    (contains? session-access access-level)))
 
 ; quick helper for access denied
 (defn access-denied
