5c21ffa7e4a836c7c6351a1043f487adfca61f9f
Oni Zenso <onicrypt@gmail.com>
2014-08-27 16:15:41 -0700

    started fresh in spec/ after runnning into RSpec complications

--- Phase II/RailService/config/application.rb
+++ Phase II/RailService/config/application.rb
@@ -6,18 +6,22 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module PhaseIii
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  ## Bundler.require(:default, :assets, Rails.env)
+end
+
+module Railservice
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Pacific Time (US & Canada)'
+    config.generators do |g|
+      g.factory_girl true
+    end
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # Enable the asset pipeline
+    config.assets.enabled = true 
   end
 end
