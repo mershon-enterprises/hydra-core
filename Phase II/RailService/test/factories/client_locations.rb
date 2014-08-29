bf770fd471dd723392a2004427308c870c68aa4e
Oni Zenso <onicrypt@gmail.com>
2014-08-29 11:17:11 -0700

    using RSpec, no longer need /test/

--- Phase II/RailService/test/factories/client_locations.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :client_location do
-    description "MyString"
-    client_id ""
-  end
-end
--- Phase II/RailService/test/models/client_location_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ClientLocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
