477bb434d3a0a2f60ba9c8af8bcd337c03885dff
Oni Zenso <onicrypt@gmail.com>
2014-09-02 10:13:53 -0700

    Modified spec_helper to accomodate OmniAuth tests\n\nThese modifications were made to properly test the OmniAuth LDAP gem.\nI also added the Skeleton for the Authenticate Controller

--- Phase II/RailService/spec/controllers/authenticates_controller_spec.rb
+++ Phase II/RailService/spec/controllers/authenticates_controller_spec.rb
@@ -18,7 +18,7 @@ require 'spec_helper'
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
 
-describe AuthenticatesController do
+describe AuthenticateController do
 
   # This should return the minimal set of attributes required to create a valid
   # Authenticate. As you add validations to Authenticate, be sure to
--- Phase II/RailService/spec/spec_helper.rb
+++ Phase II/RailService/spec/spec_helper.rb
@@ -1,7 +1,23 @@
+# OmniAuth LDAP && SimpleCov Config
+ $:.unshift File.expand_path('..', __FILE__)
+ $:.unshift File.expand_path('../../lib', __FILE__)
+require 'simplecov' 
+SimpleCov.start
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rack/test'
+require 'omniauth'
+require 'omniauth-ldap'
+
 RSpec.configure do |config|
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
+  # The following two settings are related to OmniAuth LDAP && Rack Tests
+  config.include Rack::Test::Methods
+  config.extend OmniAuth::Test::StrategyMacros, :type => :strategy
+
   # These two settings work together to allow you to limit a spec run
   # to individual examples or groups you care about by tagging them with
   # `:focus` metadata. When nothing is tagged with `:focus`, all examples
@@ -59,6 +75,13 @@ RSpec.configure do |config|
     # a real object. This is generally recommended.
     mocks.verify_partial_doubles = true
   end
+
+   
+  # If you use bundler, this might be useful to you:
+  if command -v bundle && [[ -s Gemfile ]]
+  then
+  bundle install
+  fi
 
   config.raise_errors_for_deprecations!
 =end
