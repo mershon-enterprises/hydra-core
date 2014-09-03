75f8e5fc10f883a56a37445bf667d7ef237327dc
Oni Zenso <onicrypt@gmail.com>
2014-09-03 13:50:20 -0700

    Refactored LDAP Spec
    Removed unnecessary HTTP verbs from LDAP spec and
    left comment in Authenticate spec to place auth tests
    in spec/omniauth/strategies/ldap_spec.rb
    
    Still very new to developing LDAP, suggestions very welcome

--- Phase II/RailService/spec/controllers/authenticates_controller_spec.rb
+++ Phase II/RailService/spec/controllers/authenticates_controller_spec.rb
@@ -1,78 +1,10 @@
 # TODOS:
-#   Build out Authenticate#ldap to pass the 'GET ldap' test
 #   Properly set-up 'POST create' test
 #   Complete the 'DELETE destroy' test
 require 'spec_helper'
+require 'json'
 
 describe AuthenticateController do
-
-  # This should return the minimal set of attributes required to create a valid
-  # Authenticate. As you add validations to Authenticate, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) { {  } }
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # AuthenticatesController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
-
-  describe "GET ldap" do
-    it "returns an a simple form containing :email and :password input fields" do
-      authenticate = Authenticate.create! valid_attributes
-      get :ldap, {}, valid_session
-      assigns(:authenticates).should eq([authenticate])
-    end
-  end
-
-  describe "POST create" do
-    describe "with valid params" do
-      it "creates a new Authenticate" do
-        expect {
-          post :create, {:authenticate => valid_attributes}, valid_session
-        }.to change(Authenticate, :count).by(1)
-      end
-
-      it "assigns a newly created authenticate as @authenticate" do
-        post :create, {:authenticate => valid_attributes}, valid_session
-        assigns(:authenticate).should be_a(Authenticate)
-        assigns(:authenticate).should be_persisted
-      end
-
-      it "redirects to the created authenticate" do
-        post :create, {:authenticate => valid_attributes}, valid_session
-        response.should redirect_to(Authenticate.last)
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved authenticate as @authenticate" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Authenticate.any_instance.stub(:save).and_return(false)
-        post :create, {:authenticate => {  }}, valid_session
-        assigns(:authenticate).should be_a_new(Authenticate)
-      end
-
-      it "re-renders the 'new' template" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Authenticate.any_instance.stub(:save).and_return(false)
-        post :create, {:authenticate => {  }}, valid_session
-        response.should render_template("new")
-      end
-    end
-  end
-
-  describe "DELETE destroy" do
-    it "destroys the requested authenticate" do
-      authenticate = Authenticate.create! valid_attributes
-      expect {
-        delete :destroy, {:id => authenticate.to_param}, valid_session
-      }.to change(Authenticate, :count).by(-1)
-    end
-
-    it "redirects to the authenticates list" do
-      authenticate = Authenticate.create! valid_attributes
-      delete :destroy, {:id => authenticate.to_param}, valid_session
-      response.should redirect_to(authenticates_url)
-    end
-  end
+  # Authentication tests located in
+  # ${RailsRoot}/spec/omniauth/stategies/ldap_spec.rb
 end
--- Phase II/RailService/spec/omniauth/strategies/ldap_spec.rb
+++ Phase II/RailService/spec/omniauth/strategies/ldap_spec.rb
@@ -14,42 +14,14 @@ describe "OmniAuth::Strategies::LDAP" do
   let(:app) do
     Rack::Builder.new {
       use OmniAuth::Test::PhonySession
-      use PISlixVpn, :name => 'ldap', :title => 'PI Slix VPN', :host => 'localhost:3000', :base => 'dc=score, dc=local', :uid => 'sAMAccountName', :name_proc => Proc.new {|name| name.gsub(/@.*$/,'')}
+      use PISlixVpn, :name => 'ldap', :title => 'PI Slix VPN', :host => 'localhost:3000', :base => 'dc=score, dc=local', :uid => 'pislixvpn', :name_proc => Proc.new {|name| name.gsub(/@.*$/,'')}
       run lambda { |env| [404, {'Content-Type' => 'text/plain'}, [env.key?('omniauth.auth').to_s]] }
     }.to_app
   end
 
-  let(:session) do
-    last_request.env['rack.session']
-  end
-
-  it 'should add a camelization for itself' do
-    OmniAuth::Utils.camelize('ldap').should == 'LDAP'
-  end
-
-  describe '/authentication/ldap' do
-    before(:each){ get '/authentication/ldap' }
-
-    it 'should display a form' do
-      last_response.status.should == 200
-      last_response.body.should be_include("<form")
-    end
-
-    it 'should have the callback as the action for the form' do
-      last_response.body.should be_include("action='/authentication/ldap/callback'")
-    end
-
-    it 'should have a text field for each of the fields' do
-      last_response.body.scan('<input').size.should == 2
-    end
-    it 'should have a label of the form title' do
-      last_response.body.scan('PI Slixbits VPN Form').size.should > 1
-    end
-  end
-
   describe 'post /authentication/ldap/callback' do
     before(:each) do
-      @adaptor = double(OmniAuth::LDAP::Adaptor, {:uid => 'ping'})
+      @adaptor = double(OmniAuth::LDAP::Adaptor, {:uid => "7fa1f8f6-498d-4054-9300-4fcd4fa6bb57" })
       @adaptor.stub(:filter)
       OmniAuth::LDAP::Adaptor.stub(:new).and_return(@adaptor)
     end
@@ -63,8 +35,9 @@ describe "OmniAuth::Strategies::LDAP" do
         it 'should redirect to error page' do
           post('/authentication/ldap/callback', {})
 
-          last_response.should be_redirect
+          last_response.should_not be_redirect
           last_response.headers['Location'].should =~ %r{missing_credentials}
+          last_response.body['token'].should_not be_present
         end
       end
 
@@ -74,6 +47,7 @@ describe "OmniAuth::Strategies::LDAP" do
 
           last_response.should be_redirect
           last_response.headers['Location'].should =~ %r{missing_credentials}
+          last_response.body['token'].should_not be_present
         end
       end
 
@@ -84,6 +58,7 @@ describe "OmniAuth::Strategies::LDAP" do
 
             last_response.should be_redirect
             last_response.headers['Location'].should =~ %r{missing_credentials}
+            last_response.body['token'].should_not be_present
           end
         end
 
@@ -93,14 +68,15 @@ describe "OmniAuth::Strategies::LDAP" do
 
             last_response.should be_redirect
             last_response.headers['Location'].should =~ %r{missing_credentials}
+            last_response.body['token'].should_not be_present
           end
         end
       end
 
       context "when username and password are present" do
         context "and bind on LDAP server failed" do
-          it 'should redirect to error page' do
-            post('/authentication/ldap/callback', {:username => 'ping', :password => 'password'})
+          it 'should return error HTTP status and no API token' do
+            post('/authentication/ldap/callback', {:username => 'admin@example.com', :password => 'password'})
 
             last_response.should be_redirect
             last_response.headers['Location'].should =~ %r{invalid_credentials}
@@ -109,10 +85,11 @@ describe "OmniAuth::Strategies::LDAP" do
             it 'should bind with filter' do
               @adaptor.stub(:filter).and_return('uid=%{username}')
               Net::LDAP::Filter.should_receive(:construct).with('uid=ping')
-              post('/authentication/ldap/callback', {:username => 'ping', :password => 'password'})
+              post('/authentication/ldap/callback', {:username => 'admin@example.com', :password => 'password'})
 
-              last_response.should be_redirect
+              last_response.should_not be_redirect
               last_response.headers['Location'].should =~ %r{invalid_credentials}
+              last_response.body['token'].should_not be_present
             end
           end
 
@@ -123,11 +100,12 @@ describe "OmniAuth::Strategies::LDAP" do
             @adaptor.stub(:bind_as).and_throw(Exception.new('connection_error'))
           end
 
-          it 'should redirect to error page' do
-            post('/authentication/ldap/callback', {:username => "ping", :password => "password"})
+          it 'should return error HTTP status and no API token' do
+            post('/authentication/ldap/callback', {:username => 'admin@example.com', :password => 'nil'})
 
-            last_response.should be_redirect
+            last_response.should_not be_redirect
             last_response.headers['Location'].should =~ %r{ldap_error}
+            last_response.body['token'].should_not be_present
           end
         end
       end
@@ -139,28 +117,17 @@ describe "OmniAuth::Strategies::LDAP" do
       before(:each) do
         @adaptor.stub(:filter)
         @adaptor.stub(:bind_as).and_return(Net::LDAP::Entry.from_single_ldif_string(
-      %Q{dn: cn=ping, dc=intridea, dc=com
-mail: ping@intridea.com
-givenname: Ping
-sn: Yu
-telephonenumber: 555-555-5555
-mobile: 444-444-4444
-uid: ping
-title: dev
-address: k street
-l: Washington
-st: DC
-co: U.S.A
-postofficebox: 20001
-wwwhomepage: www.intridea.com
-jpegphoto: http://www.intridea.com/ping.jpg
-description: omniauth-ldap
+      %Q{dn: cn=pislixvpn, dc=process-instruments, dc=com
+mail: admin@example.com
+givenname: Pi
+sn: Slix
+uid: 7fa1f8f6-498d-4054-9300-4fcd4fa6bb57 
 }
     ))
       end
 
       it 'should not redirect to error page' do
-        post('/authentication/ldap/callback', {:username => 'ping', :password => 'password'})
+        post('/authentication/ldap/callback', {:username => 'admin@example.com', :password => '!P1Scob70000'})
         last_response.should_not be_redirect
       end
 
@@ -168,26 +135,18 @@ description: omniauth-ldap
         it 'should bind with filter' do
           @adaptor.stub(:filter).and_return('uid=%{username}')
           Net::LDAP::Filter.should_receive(:construct).with('uid=ping')
-          post('/authentication/ldap/callback', {:username => 'ping', :password => 'password'})
+          post('/authentication/ldap/callback', {:username => 'admin@example.com', :password => '!P1Scob70000'})
 
           last_response.should_not be_redirect
         end
       end
 
       it 'should map user info to Auth Hash' do
-        post('/authentication/ldap/callback', {:username => 'ping', :password => 'password'})
+        post('/authentication/ldap/callback', {:username => 'admin@example.com', :password => '!P1Scob70000'})
         auth_hash.uid.should == 'cn=ping, dc=intridea, dc=com'
-        auth_hash.info.email.should == 'ping@intridea.com'
-        auth_hash.info.first_name.should == 'Ping'
-        auth_hash.info.last_name.should == 'Yu'
-        auth_hash.info.phone.should == '555-555-5555'
-        auth_hash.info.mobile.should == '444-444-4444'
-        auth_hash.info.nickname.should == 'ping'
-        auth_hash.info.title.should == 'dev'
-        auth_hash.info.location.should == 'k street, Washington, DC, U.S.A 20001'
-        auth_hash.info.url.should == 'www.intridea.com'
-        auth_hash.info.image.should == 'http://www.intridea.com/ping.jpg'
-        auth_hash.info.description.should == 'omniauth-ldap'
+        auth_hash.info.email.should == 'admin@example.com'
+        auth_hash.info.first_name.should == 'Pi'
+        auth_hash.info.last_name.should == 'Slix'
       end
     end
   end
