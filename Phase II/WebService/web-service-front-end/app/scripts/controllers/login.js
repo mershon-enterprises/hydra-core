7c1f4f4506a5a0b2af6c859ffc5aeec7afbea7ec
Devin Leonhart <devinleonhart@gmail.com>
2014-10-14 11:56:56 -0700

    Session is created on successful auth.

--- Phase II/WebService/web-service-front-end/app/scripts/controllers/login.js
+++ Phase II/WebService/web-service-front-end/app/scripts/controllers/login.js
@@ -8,22 +8,29 @@
  * Controller of the webServiceApp. Manages dataset data.
  */
 angular.module('webServiceApp').controller('LoginCtrl',
- function ($scope, $rootScope, AUTH_EVENTS, AuthService) {
+ function ($scope, $rootScope, AUTH_EVENTS, AuthService, Session) {
 
   $scope.credentials = {
-    username: '',
+    email: '',
     password: ''
   };
 
   $scope.login = function (credentials) {
-    AuthService.authenticate(credentials).then(function (user) {
-      $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
-      $scope.setCurrentUser(user);
-      console.log(user);
-    }, function () {
-      $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
-    });
+    AuthService.authenticate(credentials);
   };
+
+  //Listener for a successful login.
+  $scope.$on(AUTH_EVENTS.loginSuccess, function(event, args) {
+    console.log("Login Success!");
+    //TODO Display To User Auth Succeeded.
+  });
+
+  //Listener for a failed login.
+  $scope.$on(AUTH_EVENTS.loginFailed, function(event, args) {
+    console.log("Login Failed!");
+    Session.destroy;
+    //TODO Display To User Auth Failed.
+  });
 
 });
 
--- Phase II/WebService/web-service-front-end/app/scripts/services/auth.js
+++ Phase II/WebService/web-service-front-end/app/scripts/services/auth.js
@@ -1,40 +1,67 @@
 'use strict';
 
-angular.module('webServiceApp').factory('AuthService', function ($http, Session) {
+angular.module('webServiceApp').factory('AuthService',
+  function ($http, $rootScope, AUTH_EVENTS, Session) {
+
   var authService = {};
 
+  //Authenticate against the restclient. Create a new session if the restclient
+  //returns status 200, and return false otherwise.
   authService.authenticate = function (credentials) {
-    restclient.authenticate(credentials.email_address, credentials.password, function(status, response){
-      Session.create(response.data.id, response.data.user.id, response.data.user.role);
+    restclient.authenticate(credentials.email, credentials.password,
+      function(status, res){
+
+      if (status == 200) {
+
+        //Parse out the data from the restclient response.
+        var response  = JSON.parse(res);
+        var responseBody = response.response;
+        var tokenExpirationDate = response.token_expiration_date;
+        var token = response.token;
+        var email = responseBody.email_address;
+        var firstName = responseBody.first_name;
+        var lastName = responseBody.last_name;
+        var permissions = responseBody.access;
+
+        //Populate the Session singleton with the user data.
+        Session.create(tokenExpirationDate, token, email, firstName, lastName,
+          permissions);
+
+        //Broadcast to any listeners that login was successful.
+        $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+
+      }
+
+      else {
+        //Broadcast to any listeners that login has failed.
+        $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+      }
+
     });
 
-  };
-
-  authService.isAuthenticated = function () {
-    return !!Session.userId;
-  };
-
-  authService.isAuthorized = function (authorizedRoles) {
-    if (!angular.isArray(authorizedRoles)) {
-      authorizedRoles = [authorizedRoles];
-    }
-    return (authService.isAuthenticated() &&
-      authorizedRoles.indexOf(Session.userRole) !== -1);
   };
 
   return authService;
 });
 
+//Session Singleton.
 angular.module('webServiceApp').service('Session', function () {
-  this.create = function (sessionId, userId, userRole) {
-    this.id = sessionId;
-    this.userId = userId;
-    this.userRole = userRole;
+  this.create = function (tokenExpirationDate, token, email, firstName,
+    lastName, permissions) {
+    this.tokenExpirationDate = tokenExpirationDate;
+    this.token = token;
+    this.email = email;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.permissions = permissions;
   };
   this.destroy = function () {
-    this.id = null;
-    this.userId = null;
-    this.userRole = null;
+    this.tokenExpirationDate = null;
+    this.token = null;
+    this.email = null;
+    this.firstName = null;
+    this.lastName = null;
+    this.permissions = null;
   };
   return this;
 });
