20645ad734c652a7314c117b2b4d084f9415a86e
Kevin Mershon <kevin@slixbits.com>
2014-10-17 10:55:12 -0700

    Document dependencies

--- Phase II/WebService/web-service/project.clj
+++ Phase II/WebService/web-service/project.clj
@@ -5,16 +5,21 @@
                  [org.clojure/java.jdbc "0.3.4"]
                  [compojure "1.1.8"]
                  [ring/ring-core "1.2.1"]
-                 [ring/ring-jetty-adapter "1.2.1"]
-                 [ring/ring-json "0.3.1"]
-                 [postgresql "9.3-1101.jdbc4"]
-                 [org.clojars.pntblnk/clj-ldap "0.0.9"]
+                 [ring/ring-jetty-adapter "1.2.1"]      ; dev HTTP server
+                                                        ;   configuration
+                 [ring/ring-json "0.3.1"]               ; JSON response wrapping
+                 [postgresql "9.3-1101.jdbc4"]          ; database
+                 [org.clojars.pntblnk/clj-ldap "0.0.9"] ; ldap integration
                  [org.clojure/tools.logging "0.3.0"]
-                 [crypto-password "0.1.3"]
-                 [environ "1.0.0"]
-                 [com.mchange/c3p0 "0.9.2.1"]
-                 [com.novemberain/langohr "3.0.0-rc2"]]
-                 [cheshire "5.3.1"]]
+                 [crypto-password "0.1.3"]              ; hashing/crypto support
+                                                        ;   for API tokens
+                 [environ "1.0.0"]                      ; environment-based
+                                                        ;   configuration
+                 [com.mchange/c3p0 "0.9.2.1"]           ; connection pooling
+                 [com.novemberain/langohr "3.0.0-rc2"]  ; AMQP rabbitmq library
+                 [cheshire "5.3.1"]                     ; json encoding library
+                                                        ;   including dates
+                 ]
   :plugins [[lein-ring "0.8.11"]
             [lein-environ "1.0.0"]]
   :ring {:handler web-service.handler/app
