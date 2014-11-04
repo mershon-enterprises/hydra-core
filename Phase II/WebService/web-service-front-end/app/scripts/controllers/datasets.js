54c38e3fad044b3ed61092938214206f40e9804f
Devin Leonhart <devinleonhart@gmail.com>
2014-11-03 16:52:23 -0800

    Cleanup and comments

--- Phase II/WebService/web-service-front-end/app/scripts/controllers/datasets.js
+++ Phase II/WebService/web-service-front-end/app/scripts/controllers/datasets.js
@@ -4,11 +4,34 @@ angular.module('webServiceApp').controller('DatasetsCtrl', function ($scope, Res
 
     if (Session.exists()) {
 
-        $scope.data = RestService.getCacheValue('data');
+        //If any of ng-grid's options are declared seperately like this, it's
+        //so we can set up listeners on these specific options or expect them
+        //to change like variables.
+        $scope.filterOptions = {
+            filterText: "",
+            useExternalFilter: false
+        };
 
+        $scope.totalServerItems = 0;
+
+        $scope.pagingOptions = {
+            pageSizes: [25, 50, 100],
+            pageSize: 25,
+            currentPage: 1
+        };
+
+        //Options for ng-grid
+        //API : https://angular-ui.github.io/ng-grid/
         $scope.gridOptions = {
             data: 'data',
             sortInfo : { fields: ['date_created'], directions: ['desc'] },
+            enablePaging: true,
+            showFooter: true,
+            showColumnMenu: true,
+            showFilter: true,
+            totalServerItems: 'totalServerItems',
+            pagingOptions: $scope.pagingOptions,
+            filterOptions: $scope.filterOptions,
             columnDefs: [
                 {field: 'filename', displayName: 'Filename'},
                 {field: 'bytes', displayName: 'Filesize'},
@@ -20,6 +43,29 @@ angular.module('webServiceApp').controller('DatasetsCtrl', function ($scope, Res
                 {field: 'date_created', displayName: 'Creation Date'}
             ]
         };
+
+        //Retrieves data from the cache and then slices it to the requested pagination settings.
+        $scope.getPagedData = function (pageSize, page) {
+            var data = RestService.getCacheValue('data');
+            var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
+            $scope.data = pagedData;
+            $scope.totalServerItems = data.length;
+            if (!$scope.$$phase) {
+                $scope.$apply();
+            }
+        };
+
+        //Gets the data for the table for the initial page load.
+        $scope.getPagedData($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
+
+        //Watchers for when options of ng-grid are change by the user.
+        $scope.$watch('pagingOptions', function (newVal, oldVal) {
+            $scope.getPagedData($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
+        }, true);
+
+        $scope.$watch('filterOptions', function (newVal, oldVal) {
+            $scope.getPagedData($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
+        }, true);
 
     }
 
