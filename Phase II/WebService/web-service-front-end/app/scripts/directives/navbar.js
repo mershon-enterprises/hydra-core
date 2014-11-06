31a6ac4c48492985950e7573c132653006f44be8
Devin Leonhart <devinleonhart@gmail.com>
2014-11-06 11:39:43 -0800

    Removed rootscope from navbar

--- Phase II/WebService/web-service-front-end/app/scripts/directives/navbar.js
+++ Phase II/WebService/web-service-front-end/app/scripts/directives/navbar.js
@@ -4,16 +4,13 @@ angular.module('webServiceApp').directive('navbar', function() {
     return {
         restrict: 'E',
         templateUrl: 'templates/navbar.html',
-        controller: function ($rootScope, $scope, Session, EVENTS) {
-
-            $rootScope.filterText = '';
+        controller: function ($scope, Session, EVENTS) {
 
             $('.search').keyup(function() {
                 //Get current value of input field after every key press.
                 var currentValue = $(this).val();
                 $('.ngColMenu').find('input').val(currentValue);
                 $('.ngColMenu').find('input').trigger('input');
-                $rootScope.filterText = currentValue;
             });
 
             var self = this;
