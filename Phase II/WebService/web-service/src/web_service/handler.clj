d9b24c1887143c4b6ddde5e0525c487bbc1fcdc2
Kevin Mershon <kevin@slixbits.com>
2014-11-07 11:47:08 -0800

    Add client-uuid parameter to all handler and authentication functions requiring it

--- Phase II/WebService/web-service/src/web_service/handler.clj
+++ Phase II/WebService/web-service/src/web_service/handler.clj
@@ -27,25 +27,25 @@
           405))
 
 (defroutes app-routes
-  (POST "/admin-authenticate" [email_address password user_email_address]
-        (admin-authenticate email_address password user_email_address))
-  (POST "/authenticate" [email_address password]
-        (authenticate email_address password))
+  (POST "/admin-authenticate" [client_uuid email_address password user_email_address]
+        (admin-authenticate client_uuid email_address password user_email_address))
+  (POST "/authenticate" [client_uuid email_address password]
+        (authenticate client_uuid email_address password))
   (GET "/version" [] (get-version))
 
   (context
     "/access-levels" []
     (defroutes document-routes
-      (GET "/" [api_token]
-           (guard-with-user api_token access-level-list))
+      (GET "/" [api_token client_uuid]
+           (guard-with-user api_token client_uuid access-level-list))
       (PUT "/" [] (not-allowed "Update-all access levels"))
       (POST "/" [] (not-allowed "Create access level"))
       (DELETE "/" [] (not-allowed "Delete-all access levels"))
       (context
         "/:description" [description]
         (defroutes document-routes
-          (GET "/" [api_token]
-               (guard-with-user api_token access-level-get description))
+          (GET "/" [api_token client_uuid]
+               (guard-with-user api_token client_uuid access-level-get description))
           (PUT "/" [] (not-allowed "Update access level"))
           (POST "/" [] (not-allowed "Create access level"))
           (DELETE "/" [] (not-allowed "Delete access level"))))))
@@ -53,40 +53,41 @@
   (context
     "/clients" []
     (defroutes document-routes
-      (GET "/" [api_token]
-           (guard-with-user api_token client-list))
+      (GET "/" [api_token client_uuid]
+           (guard-with-user api_token client_uuid client-list))
       (PUT "/" [] (not-allowed "Update-all clients"))
-      (POST "/" [api_token name]
-            (guard-with-user api_token client-register name))
+      (POST "/" [api_token client_uuid name]
+            (guard-with-user api_token client_uuid client-register name))
       (DELETE "/" [] (not-allowed "Delete-all clients"))
       (context
         "/:name" [name]
         (defroutes document-routes
-          (GET "/" [api_token]
-               (guard-with-user api_token client-get name))
+          (GET "/" [api_token client_uuid]
+               (guard-with-user api_token client_uuid client-get name))
           (PUT "/" [] (not-implemented "Update client"))
-          (POST "/" [api_token]
-                (guard-with-user api_token client-register name))
+          (POST "/" [api_token client_uuid]
+                (guard-with-user api_token client_uuid client-register name))
           (DELETE "/" [] (not-allowed "Delete client"))
           (context
             "/locations" []
             (defroutes document-routes
-              (GET "/" [api_token]
-                   (guard-with-user api_token client-location-list name))
+              (GET "/" [api_token client_uuid]
+                   (guard-with-user api_token client_uuid client-location-list name))
               (PUT "/" [] (not-allowed "Client update-all locations"))
-              (POST "/" [api_token description]
+              (POST "/" [api_token client_uuid description]
                     (guard-with-user
-                      api_token client-location-add name description))
+                      api_token client_uuid client-location-add name description))
               (DELETE "/" [] (not-allowed "Client delete-all locations"))))))))
 
   (context
     "/data" []
     (defroutes document-routes
-      (GET "/" [api_token]
-           (guard-with-user api_token data-list))
+      (GET "/" [api_token client_uuid]
+           (guard-with-user api_token client_uuid data-list))
       (PUT "/" [] (not-allowed "Update-all data"))
-      (POST "/" [api_token uuid date_created created_by data]
+      (POST "/" [api_token client_uuid uuid date_created created_by data]
             (guard-with-user api_token
+                             client_uuid
                              data-submit
                              uuid
                              date_created
@@ -96,23 +97,25 @@
       (context
         "/:uuid" [uuid]
         (defroutes document-routes
-          (GET "/" [api_token]
-               (guard-with-user api_token data-get uuid))
+          (GET "/" [api_token client_uuid]
+               (guard-with-user api_token client_uuid data-get uuid))
           (PUT "/" [] (not-implemented "Update data"))
-          (POST "/" [api_token date_created created_by data]
+          (POST "/" [api_token client_uuid date_created created_by data]
                 (guard-with-user api_token
+                                 client_uuid
                                  data-submit
                                  uuid
                                  date_created
                                  created_by
                                  data))
-          (DELETE "/" [api_token]
-                  (guard-with-user api_token data-delete uuid))
+          (DELETE "/" [api_token client_uuid]
+                  (guard-with-user api_token client_uuid data-delete uuid))
           (context
             "/:filename" [filename]
             (defroutes document-routes
-              (GET "/" [api_token]
+              (GET "/" [api_token client_uuid]
                    (guard-file-with-user api_token
+                                         client_uuid
                                          data-get-attachment
                                          uuid
                                          filename))
@@ -123,8 +126,8 @@
   (context
     "/attachments" []
       (defroutes document-routes
-        (GET "/" [api_token]
-           (guard-with-user api_token data-list-with-attachments))
+        (GET "/" [api_token client_uuid]
+           (guard-with-user api_token client_uuid data-list-with-attachments))
         (PUT "/" [] (not-allowed "Update-all data attachments"))
         (POST "/" [] (not-allowed "Sumbit-all  data attachemnts"))
         (DELETE "/" [] (not-allowed "Delete-all data attachments"))))
@@ -132,27 +135,28 @@
   (context
     "/users" []
     (defroutes document-routes
-      (GET "/" [api_token]
-           (guard-with-user api_token user-list))
+      (GET "/" [api_token client_uuid]
+           (guard-with-user api_token client_uuid user-list))
       (PUT "/" [] (not-allowed "Update-all users"))
       (POST "/" [] (not-allowed "Register user"))
       (DELETE "/" [] (not-allowed "Delete-all users"))
       (context
         "/:email-address" [email-address]
         (defroutes document-routes
-          (GET "/" [api_token]
-               (guard-with-user api_token user-get email-address))
+          (GET "/" [api_token client_uuid]
+               (guard-with-user api_token client_uuid user-get email-address))
           (PUT "/" [] (not-implemented "Update user"))
           (POST "/" [] (not-allowed "Register user"))
           (DELETE "/" [] (not-allowed "Delete user"))
           (context
             "/access" []
             (defroutes document-routes
-              (GET "/" [api_token]
-                   (guard-with-user api_token user-access-list email-address))
+              (GET "/" [api_token client_uuid]
+                   (guard-with-user api_token client_uuid user-access-list email-address))
               (PUT "/" [] (not-implemented "User update-all access"))
-              (POST "/" [api_token description]
+              (POST "/" [api_token client_uuid description]
                     (guard-with-user api_token
+                                     client_uuid
                                      user-access-add
                                      email-address
                                      description))
