675348d8382b12751d664f284321f3ccc458349b
Devin Leonhart <devinleonhart@gmail.com>
2014-11-07 10:37:18 -0800

    Redid modal template and styling

--- Phase II/WebService/web-service-front-end/app/scripts/directives/modal-loading.js
+++ Phase II/WebService/web-service-front-end/app/scripts/directives/modal-loading.js
@@ -7,23 +7,6 @@ angular.module('webServiceApp').directive('modalLoading', function() {
       show: '='
     },
     replace: true,
-    transclude: true,
-    link: function(scope, element, attrs) {
-      scope.dialogStyle = {};
-
-      if (attrs.width) {
-          scope.dialogStyle.width = attrs.width;
-      }
-
-      if (attrs.height) {
-          scope.dialogStyle.height = attrs.height;
-      }
-
-      scope.hideModal = function() {
-        scope.show = false;
-      };
-
-    },
     templateUrl: 'templates/modal-loading.html'
   };
 });
