330e272f192b3b7e8863fbd4aed2d72684dbb456
Kevin Mershon <kevin@slixbits.com>
2014-11-19 17:54:08 -0800

    Add AWS elastic beanstalk address as production URL for now

--- Phase II/RestClient/Gruntfile.js
+++ Phase II/RestClient/Gruntfile.js
@@ -51,7 +51,7 @@ module.exports = function (grunt) {
           replacements: [
             {
               pattern: 'ENDPOINT_URL',
-              replacement: 'http://54.187.61.110:8080'
+              replacement: 'http://hydra-production.elasticbeanstalk.com/'
             }
           ]
         }
