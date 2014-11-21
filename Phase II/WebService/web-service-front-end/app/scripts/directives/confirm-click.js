c8582b4bc921a13a41c2987e10e5813c6069e11e
Devin Leonhart <devinleonhart@gmail.com>
2014-11-21 11:59:36 -0800

    Added confirmation box directive and applied it to attachment details interface

---
+++ Phase II/WebService/web-service-front-end/app/scripts/directives/confirm-click.js
@@ -0,0 +1,14 @@
+//http://stackoverflow.com/questions/18313576/confirmation-dialog-on-ng-click-angularjs
+angular.module('webServiceApp').directive('ngConfirmClick', [ function() {
+    return {
+        link: function (scope, element, attr) {
+            var msg = attr.ngConfirmClick || "Are you sure?";
+            var clickAction = attr.confirmedClick;
+            element.bind('click',function (event) {
+                if ( window.confirm(msg) ) {
+                    scope.$eval(clickAction)
+                }
+            });
+        }
+    };
+}])
