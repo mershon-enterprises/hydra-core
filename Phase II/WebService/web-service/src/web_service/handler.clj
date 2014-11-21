92c856b54a10141963a02973293991b9dff84a50
Cristopher Pascua <cristopher.pascua@gmail.com>
2014-11-20 22:15:05 -0800

    Added route paths submit-tag and delete-tag to handler.clj

--- Phase II/WebService/web-service/src/web_service/handler.clj
+++ Phase II/WebService/web-service/src/web_service/handler.clj
@@ -111,28 +111,23 @@
                                  data))
           (DELETE "/" [api_token client_uuid]
                   (guard-with-user api_token client_uuid data-set-delete uuid))
-          (context
-            "/tag" []
-            (defroutes document-routes
-              (GET "/" [] (not-allowed "Get data set primitive"))
-              (PUT "/" [] (not-allowed "Update data set primitive"))
-              (POST "/" [api_token client_uuid type description value]
-                    (guard-with-user
-                      api_token
-                      client_uuid
-                      data-set-primitive-submit
-                      uuid
-                      type
-                      description
-                      value))
-              (DELETE "/" [api_token client_uuid type description]
-                      (guard-with-user
-                        api_token
-                        client_uuid
-                        data-set-primitive-delete
-                        uuid
-                        type
-                        description))))
+          (POST "/submit-tag" [api_token client_uuid type description value]
+                (guard-with-user
+                  api_token
+                  client_uuid
+                  data-set-primitive-submit
+                  uuid
+                  type
+                  description
+                  value))
+          (DELETE "/delete-tag" [api_token client_uuid type description]
+                  (guard-with-user
+                    api_token
+                    client_uuid
+                    data-set-primitive-delete
+                    uuid
+                    type
+                    description))
           (context
             "/:filename" [filename]
             (defroutes document-routes
