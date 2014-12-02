95883b94d2ce8e0857aafe87210b5e79552935f5
Kevin Mershon <kevin@slixbits.com>
2014-12-02 15:23:17 -0800

    Fix SMTP port type and issue with environment not being defined causing null pointer

--- Phase II/WebService/well-test-identifier/src/well_test_identifier/smtp.clj
+++ Phase II/WebService/well-test-identifier/src/well_test_identifier/smtp.clj
@@ -3,10 +3,12 @@
             [postal.core :as postal]
             [environ.core :refer [env]]))
 
-(def server {:host (env :smtp-host)
-             :port (int (env :smtp-port))
-             :user (env :smtp-username)
-             :pass (env :smtp-password)})
+(defn- get-server-spec
+  []
+  {:host (env :smtp-host)
+   :port (Integer. (env :smtp-port))
+   :user (env :smtp-username)
+   :pass (env :smtp-password)})
 
 (defn- sanitize-notes
   [dirty-notes]
@@ -18,7 +20,7 @@
 
 (defn send-well-test
   [subject test-notes attachment]
-  (postal/send-message server
+  (postal/send-message (get-server-spec)
                        {:from "hydra@slixbits.com"
                         :to (env :smtp-to-address)
                         :subject subject
