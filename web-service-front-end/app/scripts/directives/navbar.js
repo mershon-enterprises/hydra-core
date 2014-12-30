79dba4c16bae9a86a02b242cf162fae16d7a4a09
Kevin Mershon <kevin@slixbits.com>
2014-12-30 10:35:30 -0800

    Don't try to create the next tag if description or value are missing

--- web-service-front-end/app/scripts/directives/navbar.js
+++ web-service-front-end/app/scripts/directives/navbar.js
@@ -59,14 +59,26 @@ angular.module('webServiceApp').directive('navbar', function() {
 
             Mousetrap.bind('tab', function() {
                 if ($rootScope.controller === 'AttachmentUpload') {
+                    // if nothing is selected, don't worry about tab presses
                     var selected = $(document.activeElement);
                     if (selected.length === 0) {
                         return true;
                     }
-                    if (selected.hasClass('tag-input') &&
-                               selected.hasClass('tag-value')) {
+
+                    // if both the description and value for a tag are set, and
+                    // we are currently focused on the tag value, when the user
+                    // presses tab, submit the current tag and create a new
+                    // blank one
+                    var tagIsDefined = ($('.tag-description').val() !== '' &&
+                                        $('.tag-value').val() !== '');
+                    if (tagIsDefined &&
+                        selected.hasClass('tag-input') &&
+                        selected.hasClass('tag-value')
+                    ) {
                         $('.fa-plus').click();
                         $('.tag-description').focus();
+                        return false;
+                    } else {
                         return false;
                     }
                 }
