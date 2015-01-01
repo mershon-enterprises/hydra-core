23a008589038f543ac47d8efce8d0cac84765402
Kevin Mershon <kevin@slixbits.com>
2015-01-01 13:08:44 -0800

    Begin implementing file replacement logic. Filter for same mime type as original file

--- web-service-front-end/app/scripts/controllers/attachment_details.js
+++ web-service-front-end/app/scripts/controllers/attachment_details.js
@@ -24,6 +24,34 @@ angular.module('webServiceApp').controller('AttachmentDetailsCtrl',
         $scope.dateCreated = null;
         $scope.createdBy = null;
         $scope.tags = [];
+        $scope.file = null;
+        $scope.fileData = null;
+
+        //Allows us to forward click events from our nice-looking styled
+        //upload button to the hidden and unstyle-able nasty-looking file
+        //input field.
+        $('.uploadButton').click(function() {
+            $('.uploadInput').click();
+        });
+
+        //Watch for updated file attachment.
+        $scope.$watch('file', function () {
+            if($scope.file) {
+
+                //Read file's binary data.
+                //http://www.html5rocks.com/en/tutorials/file/dndfiles/
+                var reader = new FileReader();
+                // Closure to capture the file information.
+                reader.onload = (function() {
+                    return function(e) {
+                        $scope.fileData = e.target.result;
+                    };
+                })($scope.file);
+                reader.readAsBinaryString($scope.file);
+
+                //TODO -- Immediately submit the updated file to the server
+            }
+        });
 
         //The user should not be visiting this view unless sent from the
         //attachment explorer controller. $rootscope.ukey will be populated if
@@ -43,6 +71,7 @@ angular.module('webServiceApp').controller('AttachmentDetailsCtrl',
                     $scope.dateCreated = success[1].date_created;
                     $scope.createdBy = success[1].created_by;
                     $scope.tags = success[1].primitive_text_data;
+                    $scope.fileMimeType = success[1].mime_type;
                 }
             },
             //Notify user if something went wrong.
