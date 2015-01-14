6fb9f96f61f6540bcde9e54b1bb6be5a8cda09c5
Devin Leonhart <devinleonhart@gmail.com>
2015-01-14 13:03:16 -0800

    Moved search omnibox to its own directive.

---
+++ web-service-front-end/app/scripts/directives/omnibox.js
@@ -0,0 +1,33 @@
+'use strict';
+
+//Omnibox Directive
+
+angular.module('webServiceApp').directive('omnibox', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/omnibox.html',
+        controller: function ($rootScope, $scope, $location, EVENTS) {
+            $('.search').keyup(function(event) {
+                 if ( event.which === 13 ) {
+                    event.preventDefault();
+                    $rootScope.$broadcast(EVENTS.newSearch, $(this).val());
+                }
+            });
+
+            $('.search-button').click(function() {
+                if($rootScope.controller !== 'AttachmentExplorer') {
+                    var ans = confirm('Cancel your work and return to the file explorer?');
+                    if(ans) {
+                        $location.path('/attachment_explorer');
+                        //forces Angular to update, since normally the $digest wont
+                        //trigger when a directive asks to change location.
+                        //http://stackoverflow.com/questions/17177492/location-path-not-working-inside-an-angular-directive
+                        $scope.$apply();
+                    }
+                }
+                $rootScope.$broadcast(EVENTS.newSearch, $('.search').val());
+            });
+        },
+        controllerAs: 'omnibox'
+    };
+});
