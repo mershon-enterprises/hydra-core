47b82211330c2a272415c86739e28afbd39fa985
Devin Leonhart <devinleonhart@gmail.com>
2015-01-20 14:45:34 -0800

    Updated advanced search front end parsing to handle quotation marks and multi-word search terms

--- web-service-front-end/app/scripts/directives/omnibox.js
+++ web-service-front-end/app/scripts/directives/omnibox.js
@@ -42,8 +42,43 @@ angular.module('webServiceApp').directive('omnibox', function() {
                 };
 
                 var params = searchString.split(' ');
+                var finalParams = [];
+                var buffer = '';
+                var single = '\'';
+                var double = '\"';
 
-                $.each(params, function(index, value){
+                //Handles the +"Kern River" use case. We need to search through
+                //the params looking for ' or " marks and combine them into
+                //single search terms after performing string.split() on spaces.
+                $.each(params, function(index, value) {
+                    console.log(value);
+                    //If you found a quote and there's nothing in the buffer, add this value to the buffer.
+                    if (((value.indexOf(single) > -1) || (value.indexOf(double) > -1)) && (buffer.length === 0)) {
+                        buffer += value;
+                    }
+                    //If you didn't find a quote and the buffer is non-empty, add this value onto the buffer with a space.
+                    else if (((value.indexOf(single) === -1) && (value.indexOf(double) === -1)) && (buffer.length > 0)) {
+                        buffer += ' ';
+                        buffer += value;
+                    }
+                    //If you found a quote and the buffer is non-empty, add this last value to the buffer, add the buffer to the finalParams, clear the buffer.
+                    else if (((value.indexOf(single) > -1) || (value.indexOf(double) > -1)) && (buffer.length > 0)) {
+                        buffer += ' ';
+                        buffer += value;
+                        buffer = buffer.replace(/["']/g, '');
+                        finalParams.push(buffer);
+                        buffer = '';
+                    }
+                    //Just add the value in.
+                    else {
+                        finalParams.push(value);
+                    }
+                });
+
+                //Then we need to go through the finalized search params and
+                //sort them by AND, OR, and NOT. Then they will be sent to the
+                //backend.
+                $.each(finalParams, function(index, value) {
                     if(value[0] === '+') {
                         searchParams.and_search_strings.push(value.slice(1));
                     }
