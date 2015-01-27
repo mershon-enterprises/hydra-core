57ca5aeb09fb90eded941bee706c414fc2345906
Devin Leonhart <devinleonhart@gmail.com>
2015-01-27 14:05:55 -0800

    Added preferences source file

---
+++ web-service-front-end/app/scripts/services/preferences.js
@@ -0,0 +1,43 @@
+'use strict';
+
+//The Preferences service will be used to maintain user-set options throughout
+//the app without cluttering things like the session, which is meant for
+//authentication/profile related tasks.
+angular.module('webServiceApp').factory('Preferences', function () {
+
+    var preferences = {};
+
+    preferences.searchParams = {
+        or_search_strings: [],
+        and_search_strings: [],
+        not_search_strings: [],
+        limit: 25,
+        offset: 0,
+        order_by: 'date_created',
+        order: 'desc'
+    };
+
+    preferences.paginationParams = {
+        currentPage: 0,
+        paginationPages: []
+    };
+
+    preferences.reset = function () {
+        preferences.searchParams = {
+            or_search_strings: [],
+            and_search_strings: [],
+            not_search_strings: [],
+            limit: 25,
+            offset: 0,
+            order_by: 'date_created',
+            order: 'desc'
+        };
+
+        preferences.paginationParams = {
+            currentPage: 0,
+            paginationPages: []
+        };
+    };
+
+    return preferences;
+});
