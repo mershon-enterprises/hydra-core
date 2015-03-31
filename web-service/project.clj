3929a2eeeea320f8b1685b7e42582ebef698679d
Kevin Mershon <kevin@slixbits.com>
2015-03-31 15:41:23 -0700

    Fix project name to match bumped service version

--- web-service/project.clj
+++ web-service/project.clj
@@ -1,4 +1,4 @@
-(defproject web-service "0.5.5-SNAPSHOT"
+(defproject web-service "0.6.0-SNAPSHOT"
   :description "REST API for hydra-core"
   :url "slixbits.com"
   :dependencies [[org.clojure/clojure "1.6.0"]
