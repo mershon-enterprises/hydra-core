204016b4514f82ed38d9e6291f25fd4d0c2f6d1f
Cristopher Pascua <cristopher.pascua@gmail.com>
2015-07-17 13:16:47 -0700

    Added 3 basic users for tests to reset.sql, Issue #6

--- database/reset.sql
+++ database/reset.sql
@@ -8,17 +8,17 @@ truncate table public.data_set cascade;
 truncate table public.user cascade;
 
 -- set up access levels
-insert into public.user_access_level (id, description) values
-(1, 'Create Attachments'),
-(2, 'Create Data'),
-(3, 'Manage Attachments'),
-(4, 'Manage Clients'),
-(5, 'Manage Data'),
-(6, 'Manage Users'),
-(7, 'View Attachments'),
-(8, 'View Clients'),
-(9, 'View Same Client Data'),
-(10, 'View Same Client Location Data');
+insert into public.user_access_level (description) values
+('Create Attachments'),
+('Create Data'),
+('Manage Attachments'),
+('Manage Clients'),
+('Manage Data'),
+('Manage Users'),
+('View Attachments'),
+('View Clients'),
+('View Same Client Data'),
+('View Same Client Location Data');
 
 -- set up clients
 insert into public.client (name) values
@@ -55,9 +55,13 @@ insert into public.client_location (id, client_id, description) values
 );
 
 -- set up users
-insert into public.user (id, email_address) values
-(1, 'admin@example.com'),
-(2, 'manager@example.com');
+insert into public.user (email_address) values
+('admin@example.com'),
+('manager@example.com'),
+('basic@example.com'),
+('basic_user_A@example.com'),
+('basic_user_B@example.com'),
+('basic_user_C@example.com');
 
 -- set up manager access for kevin
 insert into public.user_to_user_access_level (user_id, access_level_id) values
@@ -79,6 +83,22 @@ insert into public.user_to_user_access_level (user_id, access_level_id) values
 ),
 (
     (select id from public.user where email_address='manager@example.com'),
+    (select id from public.user_access_level where description='Create Data')
+),
+(
+    (select id from public.user where email_address='basic@example.com'),
+    (select id from public.user_access_level where description='Create Data')
+),
+(
+    (select id from public.user where email_address='basic_user_A@example.com'),
+    (select id from public.user_access_level where description='Create Data')
+),
+(
+    (select id from public.user where email_address='basic_user_B@example.com'),
+    (select id from public.user_access_level where description='Create Data')
+),
+(
+    (select id from public.user where email_address='basic_user_C@example.com'),
     (select id from public.user_access_level where description='Create Data')
 );
 
