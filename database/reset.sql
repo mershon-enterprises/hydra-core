4beefeef4cbf561f9ae61dc37301da4d6c921998
Cristopher Pascua <cristopher.pascua@gmail.com>
2015-07-17 22:36:09 -0700

    Added 3 basic users, correct access levels and logic to randomly share files between admin manager and 3 basic users, Issue #6

--- database/reset.sql
+++ database/reset.sql
@@ -58,10 +58,9 @@ insert into public.client_location (id, client_id, description) values
 insert into public.user (email_address) values
 ('admin@example.com'),
 ('manager@example.com'),
-('basic@example.com'),
-('basic_user_A@example.com'),
-('basic_user_B@example.com'),
-('basic_user_C@example.com');
+('basic_user_a@example.com'),
+('basic_user_b@example.com'),
+('basic_user_c@example.com');
 
 -- set up manager access for kevin
 insert into public.user_to_user_access_level (user_id, access_level_id) values
@@ -86,20 +85,32 @@ insert into public.user_to_user_access_level (user_id, access_level_id) values
     (select id from public.user_access_level where description='Create Data')
 ),
 (
-    (select id from public.user where email_address='basic@example.com'),
+    (select id from public.user where email_address='manager@example.com'),
+    (select id from public.user_access_level where description='View Attachments')
+),
+(
+    (select id from public.user where email_address='basic_user_a@example.com'),
     (select id from public.user_access_level where description='Create Data')
 ),
 (
-    (select id from public.user where email_address='basic_user_A@example.com'),
+    (select id from public.user where email_address='basic_user_a@example.com'),
+    (select id from public.user_access_level where description='View Attachments')
+),
+(
+    (select id from public.user where email_address='basic_user_b@example.com'),
     (select id from public.user_access_level where description='Create Data')
 ),
 (
-    (select id from public.user where email_address='basic_user_B@example.com'),
+    (select id from public.user where email_address='basic_user_b@example.com'),
+    (select id from public.user_access_level where description='View Attachments')
+),
+(
+    (select id from public.user where email_address='basic_user_c@example.com'),
     (select id from public.user_access_level where description='Create Data')
 ),
 (
-    (select id from public.user where email_address='basic_user_C@example.com'),
-    (select id from public.user_access_level where description='Create Data')
+    (select id from public.user where email_address='basic_user_c@example.com'),
+    (select id from public.user_access_level where description='View Attachments')
 );
 
 
