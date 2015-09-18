b6f6c65c99cd5edd5dd04bd6737c8545fff262c5
Kevin Mershon <kevin@slixbits.com>
2015-09-18 16:03:46 -0700

    Clean up whitespace and remove unnecessary nesting

--- web-service/src/web_service/user.clj
+++ web-service/src/web_service/user.clj
@@ -33,12 +33,12 @@
         can-access (or (= sanitized-email  email-address)
                        (contains? access constants/manage-users))]
     (if can-access
-      (let [user (get-user sanitized-email  )]
+      (let [user (get-user sanitized-email)]
         ; log the activity in the session
         (log-detail email-address
                     constants/session-activity
                     (str constants/session-get-user " "
-                         sanitized-email  ))
+                         sanitized-email))
         (if user
           (response {:response user})
           (not-found "User not found"))) ; inconceivable!
@@ -78,10 +78,10 @@
                             (.trim)
                             (.toLowerCase))
         access (set (get-user-access email-address))
-        can-access (or (= email-address sanitized-email )
+        can-access (or (= email-address sanitized-email)
                        (contains? access constants/manage-users))]
     (if can-access
-      (response {:response (get-user-access sanitized-email )})
+      (response {:response (get-user-access sanitized-email)})
       (access-denied constants/manage-users))))
 
 
@@ -98,50 +98,57 @@
   (let [sanitized-email (-> (or target-email-address "")
                             (.trim)
                             (.toLowerCase))
-        access (set (get-user-access email-address))]
-    (let [user-query (str "select * from public.user "
-                          "where email_address = ?::character varying(255) ")
-          access-level-query (str "select * from user_access_level "
-                                  "where description = ?::character varying(255) ")
-          user? (not (empty? (sql/query (db)
-                                       [user-query
-                                        sanitized-email])))
-          access-level? (not (empty? (sql/query (db)
-                                               [access-level-query
-                                                access-level])))]
-      (if (contains? access constants/manage-users)
-        (if (not access-level?)
-          (status (response {:response (str "User access level doesn't exists: "
-                                            access-level)}) 400)
-          (if (not user?)
-            (status (response {:response (str "Target user level doesn't exists: "
-                                              sanitized-email)}) 400)
-            (let
-              [query (str "insert into public.user_to_user_access_level "
-                          "(user_id, access_level_id) "
-                          "values ("
-                          "(select id from public.user where email_address=?), "
-                          "(select id from public.user_access_level where description=?))")
-               success (try (sql/execute! (db) [query
-                                                sanitized-email
-                                                access-level])
-                            true
-                            (catch Exception e
-                              (println (.getMessage e))
-                              (println (.getMessage (.getNextException e)))
-                              false))]
+        access (set (get-user-access email-address))
+        user-query (str "select * from public.user "
+                        "where email_address = ?::character varying(255) ")
+        access-level-query (str "select * from user_access_level "
+                                "where description = ?::character varying(255) ")
+        user? (not (empty? (sql/query (db)
+                                      [user-query
+                                       sanitized-email])))
+        access-level? (not (empty? (sql/query (db)
+                                              [access-level-query
+                                               access-level])))]
 
-              ; if we successfully created the user access level, return a "created"
-              ; status and invoke user-access-list
-              ; otherwise, return a "conflict" status
-              (if success
-                (status (user-access-list email-address sanitized-email) 201)
-                ;TODO conflict can not be determined by success of excuted query
-                ;table constraint for user_id and access_level_id is not 
-                ;implemented
-                (status (response {:response (str "User access for "
-                                                  sanitized-email
-                                                  " already exists: "
-                                                  access-level)})
-                        409)))))
-        (access-denied constants/manage-users)))))
+    (cond
+      (not (contains? access constants/manage-users))
+      (access-denied constants/manage-users)
+
+      (not access-level?)
+      (status (response {:response (str "User access level doesn't exists: "
+                                        access-level)}) 400)
+
+      (not user?)
+      (status (response {:response (str "Target user level doesn't exists: "
+                                        sanitized-email)}) 400)
+
+      :else
+      (let
+        [query (str "insert into public.user_to_user_access_level "
+                    "(user_id, access_level_id) "
+                    "values ("
+                    "(select id from public.user where email_address=?), "
+                    "(select id from public.user_access_level where description=?))")
+         success (try (sql/execute! (db) [query
+                                          sanitized-email
+                                          access-level])
+                      true
+                      (catch Exception e
+                        (println (.getMessage e))
+                        (println (.getMessage (.getNextException e)))
+                        false))]
+
+        ; if we successfully created the user access level, return a "created"
+        ; status and invoke user-access-list
+        ; otherwise, return a "conflict" status
+        (if success
+          (status (user-access-list email-address sanitized-email) 201)
+
+          ;TODO conflict can not be determined by success of excuted query
+          ;table constraint for user_id and access_level_id is not
+          ;implemented
+          (status (response {:response (str "User access for "
+                                            sanitized-email
+                                            " already exists: "
+                                            access-level)})
+                  409))))))
