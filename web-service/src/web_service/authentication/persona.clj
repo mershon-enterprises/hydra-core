8cdb91fee50388d40beb2a84f85029ab439308ef
Kevin Mershon <kevin@slixbits.com>
2015-09-30 15:42:49 -0700

    Revise Persona authenticator to properly login

--- web-service/src/web_service/authentication/persona.clj
+++ web-service/src/web_service/authentication/persona.clj
@@ -1,8 +1,12 @@
-(ns web-service.authentication.persona)
+(ns web-service.authentication.persona
+  (:require [clj-http.client :as client]
+            [cheshire.core :refer :all]
+            [environ.core :refer [env]]))
 
+; Using 'persona' authentication, anyone with a Mozilla Persona account is
+; allowed to login, but by default their permissions are very limited.
 (defn find-user
   [email-address]
-
   {:first-name    (.substring email-address
                               0
                               (.indexOf email-address
@@ -12,8 +16,19 @@
    :is-admin      false})
 
 (defn login
-  [email-address password]
+  [_ignore assertion]
 
-  ; Using 'persona' authentication, anyone with a Mozilla Persona account is
-  ; allowed to login, but by default their permissions are very limited.
-  (find-user email-address))
+  (let [{body :body}
+        (client/post "https://verifier.login.persona.org/verify"
+                     {:form-params
+                      {:assertion assertion
+                       :audience  (env :authenticator-host)}
+                      :content-type :json})
+
+        {email-address :email
+         status        :status}
+        (parse-string body true)]
+
+    (if (= "okay" status)
+      (find-user email-address)
+      nil)))
