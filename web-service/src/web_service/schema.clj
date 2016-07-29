bcc6778d9a28a85b8fce16dd96522ac45aa693b7
Kevin Mershon <kevin@mershonenterprises.com>
2016-07-28 20:43:20 -0700

    Add yesql dependency and rewrite session queries with it

--- web-service/src/web_service/schema.clj
+++ web-service/src/web_service/schema.clj
@@ -6,7 +6,19 @@
            (liquibase.resource FileSystemResourceAccessor))
   (:require [clojure.java.io :as io]
             [clojure.tools.logging :as log]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [yesql.core :refer [defqueries]]))
+
+(def db-spec {:classname   "org.postgresql.Driver"
+              :subprotocol "postgresql"
+              :subname     (str "//" (env :db-host)
+                                ":"  (env :db-port)
+                                "/"  (env :db-name))
+              :user        (env :db-user)
+              :password    (env :db-password)})
+
+(defqueries "queries/session.sql"     {:connection db-spec})
+
 
 (defn- liquibase-instance
   "Generate liquibase instance using environment variables."
