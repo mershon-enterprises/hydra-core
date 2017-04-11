4900efd3683eed33fe7ff858b02c9523cb466bc1
Kevin Mershon <kevin@mershonenterprises.com>
2017-04-10 18:57:58 -0700

    Create data-set submit test for multiple primitives

--- web-service/test/hydra/test/routes/data.clj
+++ web-service/test/hydra/test/routes/data.clj
@@ -3,22 +3,41 @@
             [ring.mock.request :as mock]
             [cheshire.core :refer :all]
 
+            [hydra.constants :as constants]
+
             [web-service.user-helpers :refer [get-user-access]]
             [web-service.authentication :refer [guard-with-user]]
+            [web-service.session :refer [log-detail]]
             [web-service.handler :refer [app]]))
 
-(deftest test-add-data
-  (with-redefs-fn
-    {#'web-service.authentication/guard-with-user
-     (fn [api-token client-uuid fun & args]
-       (apply fun (:email_address "test@user" args)))}
-    (testing "add primitive data"
-      (let [response (app (mock/request :post "/data"
-                                        {:uuid nil
-                                         :date_created nil
-                                         :created_by "me"
-                                         :data [{:type "boolean" :value false}]}))
-            json-body (parse-string (:body response) true)]
+(deftest test-submit-data
+  (let [data-set [{:description "User name" :type "text"    :value "adminuser"}
+                  {:description "Car Make"  :type "text"    :value "Ford"}
+                  {:description "Car Model" :type "text"    :value "Mustang"}
+                  {:description "Car Year"  :type "integer" :value 1976}]
+        submit-data {:uuid "85cb0d05-71f3-033f-b586-6cbb355ca2f5"
+                     :date_created (java.util.Date.)
+                     :created_by "admin@example.com"
+                     :data (generate-string data-set)}]
+    (with-redefs-fn
+     {#'web-service.data/data-set-submit
+      (fn [email uuid date-created created-by data]
+        ; return the submit data back
+        submit-data)
+      #'web-service.user-helpers/get-user-access
+      (fn [email-address]
+        [constants/manage-data])
+      #'web-service.authentication/guard-with-user
+      (fn [api-token client-uuid fun & args]
+        (apply fun (:email_address "test@user") args))}
+
+     #(testing "add primitive data"
+       (let [response (app (mock/request :post "/data" submit-data))
+             json-data (parse-string (:data response) true)]
         (is (= 200 (:status response)))
 
-        (contains? json-body true)))))
+        (let [all-keys [:headers :body :data :status]]
+          (doseq [k all-keys]
+            (is (true? (contains? response k)))))
+
+        (is (= data-set json-data)))))))
