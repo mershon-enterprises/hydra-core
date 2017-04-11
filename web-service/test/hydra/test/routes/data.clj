c4fac988a3596b4c2e1a810db17025ef0038f4df
Kevin Mershon <kevin@mershonenterprises.com>
2017-04-10 19:51:31 -0700

    Add unit test for getting data set by uid

--- web-service/test/hydra/test/routes/data.clj
+++ web-service/test/hydra/test/routes/data.clj
@@ -15,7 +15,7 @@
                   {:description "Car Make"  :type "text"    :value "Ford"}
                   {:description "Car Model" :type "text"    :value "Mustang"}
                   {:description "Car Year"  :type "integer" :value 1976}]
-        submit-data {:uuid "85cb0d05-71f3-033f-b586-6cbb355ca2f5"
+        submit-data {:uuid "85cb0d05-71f3-033f-b586-6cbb355ca2f3"
                      :date_created (java.util.Date.)
                      :created_by "admin@example.com"
                      :data (generate-string data-set)}]
@@ -41,3 +41,36 @@
             (is (true? (contains? response k)))))
 
         (is (= data-set json-data)))))))
+
+(deftest test-get-data
+  (let [data-set [{:description "User name" :type "text"    :value "adminuser"}
+                  {:description "Car Make"  :type "text"    :value "Ford"}
+                  {:description "Car Model" :type "text"    :value "Mustang"}
+                  {:description "Car Year"  :type "integer" :value 1976}]
+        response-data {:uuid "85cb0d05-71f3-033f-b586-6cbb355ca2f4"
+                       :date_created (java.util.Date.)
+                       :location nil
+                       :client   nil
+                       :data data-set
+                       :created_by "test@user"}]
+
+    (with-redefs-fn
+     {#'web-service.data/data-set-get
+      (fn [email-address uuid & args]
+        ; return the submit data array back
+        {:response response-data})
+      #'web-service.user-helpers/get-user-access
+      (fn [email-address]
+        [constants/manage-data])
+      #'web-service.authentication/guard-with-user
+      (fn [api-token client-uuid fun & args]
+        (apply fun (:email_address "test@user") args))}
+
+     #(testing "get primitive data"
+       (let [response (app (mock/request :get "/data/85cb0d05-71f3-033f-b586-6cbb355ca2f4"))
+             json-data (:response response)]
+        (is (= 200 (:status response)))
+        (let [all-keys [:headers :body :status]]
+          (doseq [k all-keys]
+            (is (true? (contains? response k)))))
+        (is (= response-data json-data)))))))
